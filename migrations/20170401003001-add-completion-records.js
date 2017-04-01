57dc86d0f480a45030d0a109b8bd48816c35c380
Domenic Murtari <domenic.murtari@readytalk.com>
2017-04-01 08:06:15 -0600

    Migration for completion records

---
+++ migrations/20170401003001-add-completion-records.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    queryInterface.addColumn('Offerings', 'requirements', {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: false,
+      defaultValue: []
+    });
+
+    queryInterface.addColumn('Assignments', 'completions', {
+      type: Sequelize.JSON,
+      allowNull: false,
+      defaultValue: {}
+    });
+  },
+
+  down: function (queryInterface) {
+    queryInterface.removeColumn('Offerings', 'requirements');
+    queryInterface.removeColumn('Assignments', 'completions');
+  }
+};
