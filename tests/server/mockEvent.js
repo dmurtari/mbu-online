8b3d4cee2045486906f5689608ea10389c2ccf9a
Domenic Murtari <domenic.murtari@gmail.com>
2017-05-07 12:33:45 -0600

    Create API for creating mock events

---
+++ tests/server/mockEvent.js
@@ -0,0 +1,14 @@
+var testEvents = require('./testEvents').events;
+var testBadges = require('./testBadges').badges;
+var testPurchasables = require('./testPurchasables').purchasables;
+
+var Models = require('../../src/models');
+
+module.exports = {
+  createPopulatedEvent: function () {
+
+  },
+  cleanEvent: function () {
+
+  }
+}
