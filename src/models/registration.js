f9a77f4e5916aa9e645a4dd966101eec8fbae030
--global <--global>
2017-09-27 20:20:39 -0600

    Upgraded packages

--- src/models/registration.js
+++ src/models/registration.js
@@ -22,57 +22,57 @@ module.exports = function (sequelize, DataTypes) {
     }
   }, {
     underscored: true,
-    instanceMethods: {
-      projectedCost: function () {
-        var totalCost = 0;
-        var registration = this;
-
-        return registration.getPurchases()
-          .then(function (purchases) {
-            totalCost = _.reduce(purchases, function (sum, purchase) {
-              return sum + (Number(purchase.price) * Number(purchase.Purchase.quantity));
-            }, totalCost);
-
-            return registration.getPreferences();
-          })
-          .then(function (preferences) {
-            totalCost = _.reduce(preferences, function (sum, preference) {
-              return sum + Number(preference.price);
-            }, totalCost);
-
-            return sequelize.models.Event.findById(registration.event_id);
-          })
-          .then(function (event) {
-            totalCost += Number(event.price);
-            return totalCost;
-          });
-      },
-      actualCost: function () {
-        var totalCost = 0;
-        var registration = this;
-
-        return registration.getPurchases()
-          .then(function (purchases) {
-            totalCost = _.reduce(purchases, function (sum, purchase) {
-              return sum + (Number(purchase.price) * Number(purchase.Purchase.quantity));
-            }, totalCost);
-
-            return registration.getAssignments();
-          })
-          .then(function (assignments) {
-            totalCost = _.reduce(assignments, function (sum, assignment) {
-              return sum + Number(assignment.price);
-            }, totalCost);
-
-            return sequelize.models.Event.findById(registration.event_id);
-          })
-          .then(function (event) {
-            totalCost += Number(event.price);
-            return totalCost;
-          });
-      }
-    }
   });
+
+  Registration.prototype.projectedCost = function () {
+    var totalCost = 0;
+    var registration = this;
+
+    return registration.getPurchases()
+      .then(function (purchases) {
+        totalCost = _.reduce(purchases, function (sum, purchase) {
+          return sum + (Number(purchase.price) * Number(purchase.Purchase.quantity));
+        }, totalCost);
+
+        return registration.getPreferences();
+      })
+      .then(function (preferences) {
+        totalCost = _.reduce(preferences, function (sum, preference) {
+          return sum + Number(preference.price);
+        }, totalCost);
+
+        return sequelize.models.Event.findById(registration.event_id);
+      })
+      .then(function (event) {
+        totalCost += Number(event.price);
+        return totalCost;
+      });
+  }
+
+  Registration.prototype.actualCost = function () {
+    var totalCost = 0;
+    var registration = this;
+
+    return registration.getPurchases()
+      .then(function (purchases) {
+        totalCost = _.reduce(purchases, function (sum, purchase) {
+          return sum + (Number(purchase.price) * Number(purchase.Purchase.quantity));
+        }, totalCost);
+
+        return registration.getAssignments();
+      })
+      .then(function (assignments) {
+        totalCost = _.reduce(assignments, function (sum, assignment) {
+          return sum + Number(assignment.price);
+        }, totalCost);
+
+        return sequelize.models.Event.findById(registration.event_id);
+      })
+      .then(function (event) {
+        totalCost += Number(event.price);
+        return totalCost;
+      });
+  }
 
   return Registration;
 };
