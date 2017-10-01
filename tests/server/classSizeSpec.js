9c097cc6fdd64b858eb0f537e3e6128f61cd7b56
Domenic Murtari <domenic.murtari@gmail.com>
2017-10-01 15:06:35 -0600

    More setup

--- tests/server/classSizeSpec.js
+++ tests/server/classSizeSpec.js
@@ -7,30 +7,34 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var utils = require('./testUtils');
+var testScouts = require('./testScouts');
 
 describe.only('Class sizes', function () {
-  var adminToken, badges, events;
+  var badges, events;
+  var generatedUsers, generatedScouts, generatedOfferings;
+  var scoutId;
+  var registrationIds = [];
   var badId = '123456789012345678901234';
 
   before(function (done) {
     utils.dropDb(done);
   });
 
+  beforeEach(function (done) {
+    utils.dropTable(['Event', 'Offering', 'Badge', 'Assignment', 'Registration'], done);
+  });
+
   before(function (done) {
-    this.timeout(5000);
-    utils.generateToken('admin', function (err, token) {
+    this.timeout(10000);
+    utils.generateTokens(['admin', 'teacher', 'coordinator'], function (err, generatedTokens) {
       if (err) return done(err);
-      adminToken = token;
+      generatedUsers = generatedTokens;
       return done();
     });
   });
 
   beforeEach(function (done) {
-    utils.dropTable(['Event', 'Offering', 'Badge'], done);
-  });
-
-  beforeEach(function (done) {
-    utils.createBadges(adminToken, function (err, items) {
+    utils.createBadges(generatedUsers.admin.token, function (err, items) {
       if (err) return done(err);
       badges = items;
       return done();
@@ -38,12 +42,53 @@ describe.only('Class sizes', function () {
   });
 
   beforeEach(function (done) {
-    utils.createEvents(adminToken, function (err, items) {
+    utils.createEvents(generatedUsers.admin.token, function (err, items) {
 
       if (err) return done(err);
       events = items;
       return done();
     });
+  });
+
+  beforeEach(function (done) {
+    utils.createScoutsForUser(generatedUsers.coordinator, testScouts(5), generatedUsers.coordinator.token, function (err, scouts) {
+      if (err) return done(err);
+      generatedScouts = scouts;
+      return done();
+    });
+  });
+
+  beforeEach(function (done) {
+    scoutId = generatedScouts[0].id;
+    registrationIds = [];
+    async.series([
+      function (cb) {
+        request.post('/api/scouts/' + scoutId + '/registrations')
+          .set('Authorization', generatedUsers.coordinator.token)
+          .send({
+            event_id: events[0].id
+          })
+          .expect(status.CREATED)
+          .end(function (err, res) {
+            if (err) return done(err);
+            registrationIds.push(res.body.registration.id);
+            return cb();
+          });
+      },
+      function (cb) {
+        request.post('/api/scouts/' + scoutId + '/registrations')
+          .set('Authorization', generatedUsers.coordinator.token)
+          .send({
+            event_id: events[1].id
+          })
+          .expect(status.CREATED)
+          .end(function (err, res) {
+            if (err) return done(err);
+            registrationIds.push(res.body.registration.id);
+            return cb();
+          });
+      }
+    ], done);
   });
 
   after(function (done) {
@@ -67,7 +112,7 @@ describe.only('Class sizes', function () {
 
     it('should default to 20 as the size limit', function (done) {
       request.post('/api/events/' + events[0].id + '/badges')
-        .set('Authorization', adminToken)
+        .set('Authorization', generatedUsers.admin.token)
         .send(postData)
         .expect(status.CREATED)
         .end(function (err, res) {
@@ -83,7 +128,7 @@ describe.only('Class sizes', function () {
       postData.offering.size_limit = 30;
 
       request.post('/api/events/' + events[0].id + '/badges')
-        .set('Authorization', adminToken)
+        .set('Authorization', generatedUsers.admin.token)
         .send(postData)
         .expect(status.CREATED)
         .end(function (err, res) {
@@ -99,7 +144,7 @@ describe.only('Class sizes', function () {
       postData.offering.size_limit = -1;
 
       request.post('/api/events/' + events[0].id + '/badges')
-        .set('Authorization', adminToken)
+        .set('Authorization', generatedUsers.admin.token)
         .send(postData)
         .expect(status.BAD_REQUEST, done);
     });
@@ -108,9 +153,41 @@ describe.only('Class sizes', function () {
       postData.offering.size_limit = 'hello';
 
       request.post('/api/events/' + events[0].id + '/badges')
-        .set('Authorization', adminToken)
+        .set('Authorization', generatedUsers.admin.token)
         .send(postData)
         .expect(status.BAD_REQUEST, done);
+    });
+  });
+
+  describe('when an offering exists', function () {
+    var offering;
+
+    beforeEach(function (done) {
+      var postData = {
+        badge_id: badges[1].id,
+        offering: {
+          duration: 1,
+          periods: [1, 2, 3],
+          price: '10.00',
+          requirements: ['1', '2', '3a', '3b'],
+          size_limit: 1
+        }
+      };
+
+      request.post('/api/events/' + events[0].id + '/badges')
+        .set('Authorization', generatedUsers.admin.token)
+        .send(postData)
+        .expect(status.CREATED)
+        .end(function (err, res) {
+          if (err) return done(err);
+          var event = res.body.event;
+          offering = event.offerings[0];
+          return done();
+        });
+    });
+
+    it('should allow joining if under the limit', function (done) {
+      done();
     });
   });
 });
