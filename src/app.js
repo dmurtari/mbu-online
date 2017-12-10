8e514fbd17eb321de0a389cb0c7a4851492120c5
--global <domenic.murtari@gmail.com>
2017-12-10 16:42:57 -0700

    Morgan logger for http

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
 var passport = require('passport');
 var path = require('path');
 
@@ -12,6 +12,7 @@ var app = express();
 var history = require('connect-history-api-fallback');
 var env = process.env.NODE_ENV || 'development';
 var port = process.env.PORT || 3000;
+var morganFormat = ':method :url :status :res[content-length] - :response-time ms';
 
 app.use(history({
   // verbose: true
@@ -21,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 if (env === 'development') {
-  app.use(logger('dev'));
+  app.use(morgan(morganFormat));
   app.use(function (req, res, next) {
     setTimeout(function () {
       next();
@@ -30,6 +31,7 @@ if (env === 'development') {
 }
 
 if (env === 'production') {
+  app.use(morgan(morganFormat));
   app.use(express.static(path.join(__dirname, '../node_modules/mbu-frontend/dist')));
 }
 
